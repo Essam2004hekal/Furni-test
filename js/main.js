e0ec31070c18a9faa2c1542d7016ca027ca3f4ce
Essam2004hekal <essahaikal2004@gmail.com>
2024-05-08 08:58:01 +0300

    logo edit

--- js/main.js
+++ js/main.js
@@ -1,51 +1,53 @@
-    /* Initialize Swiper */
-    var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1,
-    spaceBetween: 30,
-    loop: true,
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-    autoplay: {
-        delay: 3000,
-    },
-    });
+/* Initialize Swiper */
+var swiper = new Swiper(".mySwiper", {
+slidesPerView: 1,
+spaceBetween: 30,
+loop: true,
+pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+},
+navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+},
+autoplay: {
+    delay: 3000,
+},
+});
 
-    countCart = document.querySelector(".count-item");
-    countCart.innerHTML = JSON.parse(localStorage.getItem("id")).length;
-    // form validation send email
-    function SendMail() {
-    let params = {
-        name: document.getElementById("f_name").value,
-        email: document.getElementById("email").value,
-        message: document.getElementById("message").value,
-    };
-    emailjs
-        .send("service_l7kdz47", "template_va47ber", params)
-        .then(alert(" Email Sent!!"));
-    }
+countCart = document.querySelector(".count-item");
+countCart.innerHTML = JSON.parse(localStorage.getItem("id")).length;
+
+
+// form validation send email
+function SendMail() {
+let params = {
+    name: document.getElementById("f_name").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value,
+};
+emailjs
+    .send("service_l7kdz47", "template_va47ber", params)
+    .then(alert(" Email Sent!!"));
+}
 
 // Add to cart
 let items_in_cart = document.querySelector(".items_in_cart");
 
 // Remove Product
-    function removeProduct(param) {
-    Array.from(document.querySelectorAll(".remove")).map((item) => {
-        item.addEventListener("click", () => {
-            console.log(item.dataset.id);
-            const newData = JSON.parse(localStorage.getItem("id")).filter((e) => {
-            return item.dataset.id != e.id;
-        });
-        localStorage.setItem("id", JSON.stringify(newData));
-        item.parentNode.parentNode.remove();
-        });
+function removeProduct(param) {
+Array.from(document.querySelectorAll(".remove")).map((item) => {
+    item.addEventListener("click", () => {
+        console.log(item.dataset.id);
+        const newData = JSON.parse(localStorage.getItem("id")).filter((e) => {
+        return item.dataset.id != e.id;
     });
-    }
+    localStorage.setItem("id", JSON.stringify(newData));
+    item.parentNode.parentNode.remove();
+    });
+});
+}
 
 let total_price = document.getElementsByClassName("total-price");
 let totalValue = localStorage.getItem("totalValue");
@@ -59,32 +61,32 @@ document.getElementsByClassName("total-price").innerHTML = totalValue;
 
 
 function getCartItems() {
-    let items_c = "";
-    product_cart = JSON.parse(localStorage.getItem("id"));
-    if (product_cart) {
-        product_cart.map((item) => {
-        items_c += `
-                <tr class="table-row">
-                    <td class="w-25"><img src="${item.img}" class="w-50" alt="" /></td>
-                    <td class="name">  ${item.name}</td>
-                    <td class="Price">${item.price}</td>
-                    <td>
-                    <div class="d-flex align-items-center justify-content-evenly">
-                        <button class="border-0 fs-5 increment">+</button>
-                            <span class="border btn fs-5 m-0 quantity"> 1 </span>
-                        <button class=" border-0 fs-5 fw-bold decrement">-</button>
-                    </div>
-                    </td>
-                    <td class="total-price" id="total_price_id">${item.price}</td>
-                    <td>
-                    <button class="btn remove" data-id="${item.id}">×</button>
+let items_c = "";
+product_cart = JSON.parse(localStorage.getItem("id"));
+if (product_cart) {
+    product_cart.map((item) => {
+    items_c += `
+            <tr class="table-row">
+                <td class="w-25"><img src="${item.img}" class="w-50" alt="" /></td>
+                <td class="name">  ${item.name}</td>
+                <td class="Price">${item.price}</td>
+                <td>
+                <div class="d-flex align-items-center justify-content-evenly">
+                    <button class="border-0 fs-5 increment">+</button>
+                        <span class="border btn fs-5 m-0 quantity"> 1 </span>
+                    <button class=" border-0 fs-5 fw-bold decrement">-</button>
+                </div>
                 </td>
-                </tr>
-                `;
-        });
-        items_in_cart.innerHTML = items_c;
+                <td class="total-price" id="total_price_id">${item.price}</td>
+                <td>
+                <button class="btn remove" data-id="${item.id}">×</button>
+            </td>
+            </tr>
+            `;
+    });
+    items_in_cart.innerHTML = items_c;
 
-        removeProduct();
-    }
-    };
-    getCartItems()
+    removeProduct();
+}
+};
+getCartItems()
